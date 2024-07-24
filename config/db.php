4adf67b219badddf4f7b00eb12c07ffe6f94d482
Tirth <tirthno.1gamer@gmail.com>
2024-07-24 12:15:11 +0530

    task 2 php

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+// Enable us to use Headers
+ob_start();
+
+// Set sessions
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'mydb';
+
+$connection = mysqli_connect($hostname, $username, $password, $dbname) or die('Database connection not established.');
